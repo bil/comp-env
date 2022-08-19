a150f669abaaa4cbf44f4d434e6813f247c51af7
Paul Nuyujukian <38359213+bil-paul@users.noreply.github.com>
2022-08-18 21:03:37 -0700

    drop sailor, run as root add meson to scipy build

--- containerfile/run/scipy.sh
+++ containerfile/run/scipy.sh
@@ -10,6 +10,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -qq install build-essential gfortran cmake
 # python3-dev
 apt-get -qq install python3-dev
+# need meson for scipy as of 1.9.0
 
 # Intel MKL
 echo "debconf libmkl-rt/use-as-default-blas-lapack select true" | debconf-set-selections
@@ -19,7 +20,7 @@ echo "MKL_THREADING_LAYER=GNU"		>> /etc/environment
 
 # numpy & scipy
 pip install cython pybind11
-NPY_NUM_BUILD_JOBS=$NUM_CPUS pip install numpy --no-binary numpy
+NPY_NUM_BUILD_JOBS=$NUM_CPUS pip install --no-binary numpy
 NPY_NUM_BUILD_JOBS=$NUM_CPUS pip install scipy --no-binary scipy
 
 # core python libraries
